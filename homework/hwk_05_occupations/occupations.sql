1a6d06420296316371ac897c1b87658a8d154eb8
13Lena <hamiltonlena21@gmail.com>
2023-03-14 10:50:39 -0600

    activity12 folder and docker path

--- homework/hwk_05_occupations/occupations.sql
+++ homework/hwk_05_occupations/occupations.sql
@@ -29,10 +29,8 @@ SELECT DISTINCT job_family FROM Occupations ORDER BY job_family;
 SELECT COUNT(DISTINCT job_family) FROM Occupations;
 
 -- TODO: d) the total number of occupations per job family in alphabetical order of job family.
-SELECT job_family, COUNT(*) as num_occupations 
-FROM Occupations 
-GROUP BY job_family 
-ORDER BY job_family;
+SELECT job_family, COUNT(*) as num_occupations FROM Occupations 
+GROUP BY job_family ORDER BY job_family;
 
 -- TODO: e) the number of occupations in the "Computer and Mathematical" job family (expect 38)
 SELECT COUNT(*) FROM Occupations WHERE job_family = 'Computer and Mathematical';
@@ -40,13 +38,9 @@ SELECT COUNT(*) FROM Occupations WHERE job_family = 'Computer and Mathematical';
 -- BONUS POINTS
 
 -- TODO: f) an alphabetical list of occupations in the "Computer and Mathematical" job family.
-SELECT occupation_title
-FROM occupations
-WHERE job_family = 'Computer and Mathematical'
-ORDER BY occupation_title ASC;
+SELECT occupation_title FROM occupations
+WHERE job_family = 'Computer and Mathematical' ORDER BY occupation_title ASC;
 
 -- TODO: g) an alphabetical list of occupations in the "Computer and Mathematical" job family that begins with the word "Database"
-SELECT occupation
-FROM Occupations
-WHERE job_family = 'Computer and Mathematical' AND occupation LIKE 'Database%'
-ORDER BY occupation;
+SELECT occupation FROM Occupations
+WHERE job_family = 'Computer and Mathematical' AND occupation LIKE 'Database%' ORDER BY occupation;
