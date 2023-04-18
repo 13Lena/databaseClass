1286d7e1e2a2386bbe9fe959ce92d901cd5dd948
13Lena <hamiltonlena21@gmail.com>
2023-04-18 16:28:19 -0600

    test redo

---
+++ projects/test_redo/exam2.sql
@@ -0,0 +1,16 @@
+-- CS3810: Principles of Database Systems
+-- Instructor: Thyago Mota
+-- Student: Lena Hamilton
+-- Description: Exam 2 Take Home
+
+CREATE DATABASE exam2;
+
+\c exam2
+
+-- todo 1. Create all the tables after normalization and populate them with the information from the (unnormalized) table.
+
+-- todo2. Create and populate table Employees. Create trigger function doit and trigger trigger_doit. Simulate the insert of the new employee named Paul and copy and paste the result of querying Paul's salary (as a comment in the script).
+
+-- todo3. Create and populate table Visitors. Write all SQL statements asked in the exam (a, b, and c).
+
+-- todo4. Create and populate tables Specialties and MadScientists. Write all SQL statements asked in the exam (a, b, and c). Copy and paste the result of query c (as a comment in the script).
